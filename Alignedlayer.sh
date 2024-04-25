a210812f3233e32aaed3186ba3a8c3b6a58cfbb3
snakeeeeeeeee <316161583@qq.com>
2024-04-26 01:23:31 +0800

    增加判断时候存在快照在$HOME目录下

--- Alignedlayer.sh
+++ Alignedlayer.sh
@@ -5,11 +5,11 @@ SCRIPT_PATH="$HOME/Alignedlayer.sh"
 
 # 节点安装功能
 function install_node() {
-    
+
     # 更新和安装依赖
     sudo apt update && sudo apt upgrade -y
     sudo apt install curl git jq lz4 build-essential -y
-    
+
     # 安装 Go
     rm -rf $HOME/go
     sudo rm -rf /usr/local/go
@@ -17,31 +17,31 @@ function install_node() {
     echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
     source $HOME/.bash_profile
     go version
-    
+
     # 下载并安装 Aligned Layer 二进制文件
     cd $HOME
     wget https://github.com/yetanotherco/aligned_layer_tendermint/releases/download/v0.1.0/alignedlayerd
     chmod +x alignedlayerd
     sudo mv alignedlayerd /usr/local/bin/
-    
+
     # 创建节点名称
     read -p "节点名称: " MONIKER
     # 配置节点和创世文件
     alignedlayerd init $MONIKER --chain-id alignedlayer
-    
+
     # 安装创世文件
    curl -Ls https://raw.githubusercontent.com/molla202/AlignedLayer/main/genesis.json > $HOME/.alignedlayer/config/genesis.json
    curl -Ls https://raw.githubusercontent.com/molla202/AlignedLayer/main/addrbook.json > $HOME/.alignedlayer/config/addrbook.json
-    
+
     # 设置配置文件
     SEEDS="d1d43cc7c7aef715957289fd96a114ecaa7ba756@testnet-seeds.nodex.one:24210"
     PERSISTENT_PEERS="125b4260951111e1d7111c071011aec6d24f2087@148.251.82.6:26656,74af08a0cf53d78e3a071c944b355cae95c1c1ef@37.60.243.112:26656,797d6ad9a64abd63b785ce81c75ee7397a590786@213.199.62.101:26656,33a338aef4f9e887571fe7e2baf9dd5baa43e9a2@47.236.180.181:26656,0468a823477832e2dd17c94834ac639ac1929860@213.199.39.156:26656"
     MINIMUM_GAS_PRICES="0.0001stake"
-    
+
     sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/.alignedlayer/config/config.toml
     sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PERSISTENT_PEERS\"|" $HOME/.alignedlayer/config/config.toml
     sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"$MINIMUM_GAS_PRICES\"|" $HOME/.alignedlayer/config/app.toml
-    
+
     # 设置启动服务
     sudo tee /etc/systemd/system/alignedlayerd.service > /dev/null <<EOF
 [Unit]
@@ -56,17 +56,26 @@ LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-    
-    # 下载快照
-    wget $(curl -s https://services.staketab.org/backend/aligned-testnet/ | jq -r .snap_link)
-    tar -xf $(curl -s https://services.staketab.org/backend/aligned-testnet/ | jq -r .snap_filename) -C $HOME/.alignedlayer/data/
-    
+
+    # 获取快照文件名
+    SNAP_FILENAME=$(curl -s https://services.staketab.org/backend/aligned-testnet/ | jq -r .snap_filename)
+
+    # 检查$HOME目录下快照文件是否已经存在
+    if [ ! -f "$HOME/$SNAP_FILENAME" ]; then
+        echo "快照文件不存在，开始下载..."
+        # 下载快照
+        wget $(curl -s https://services.staketab.org/backend/aligned-testnet/ | jq -r .snap_link) -O "$HOME/$SNAP_FILENAME"
+    fi
+
+    # 解压快照文件到指定的数据目录
+    tar -xf "$HOME/$SNAP_FILENAME" -C "$HOME/.alignedlayer/data/"
+
     sudo systemctl daemon-reload
     sudo systemctl enable alignedlayerd
     sudo systemctl start alignedlayerd
-    
+
     echo "====================== 部署完成 ==========================="
-        
+
 }
 
 # 创建钱包
@@ -80,8 +89,8 @@ function add_wallet() {
 function add_validator() {
     echo "钱包余额需大于1050000stake，否则创建失败..."
     read -r -p "请输入你的钱包名称: " wallet_name
-    cd $HOME 
-    wget -O setup_validator.sh https://raw.githubusercontent.com/yetanotherco/aligned_layer_tendermint/main/setup_validator.sh 
+    cd $HOME
+    wget -O setup_validator.sh https://raw.githubusercontent.com/yetanotherco/aligned_layer_tendermint/main/setup_validator.sh
     chmod +x setup_validator.sh && bash setup_validator.sh $wallet_name 1050000stake
 }
 
@@ -94,7 +103,7 @@ function import_wallet() {
 # 查询余额
 function check_balances() {
     read -p "请输入钱包地址: " wallet_address
-    alignedlayerd query bank balances "$wallet_address" 
+    alignedlayerd query bank balances "$wallet_address"
 }
 
 # 查看节点同步状态
@@ -109,19 +118,19 @@ function check_service_status() {
 
 # 节点日志查询
 function view_logs() {
-    sudo journalctl -f -u alignedlayerd.service 
+    sudo journalctl -f -u alignedlayerd.service
 }
 
 # 删除节点
 function uninstall_node() {
-    sudo systemctl stop alignedlayerd 
-    sudo systemctl disable alignedlayerd 
-    sudo rm /etc/systemd/system/alignedlayerd.service 
-    sudo systemctl daemon-reload 
-    rm -rf $HOME/.alignedlayerd 
-    rm -rf $HOME/alignedlayer 
-    sudo rm -rf $(which alignedlayerd) 
-    rm -rf aligned_layer_tendermint 
+    sudo systemctl stop alignedlayerd
+    sudo systemctl disable alignedlayerd
+    sudo rm /etc/systemd/system/alignedlayerd.service
+    sudo systemctl daemon-reload
+    rm -rf $HOME/.alignedlayerd
+    rm -rf $HOME/alignedlayer
+    sudo rm -rf $(which alignedlayerd)
+    rm -rf aligned_layer_tendermint
     rm -rf .alignedlayer
 }
 
@@ -149,26 +158,26 @@ function main_menu() {
         echo "2. 创建钱包"
         echo "3. 导入钱包"
         echo "4. 查看余额"
-        echo "5. 同步状态"    
+        echo "5. 同步状态"
         echo "6. 创建验证者"
         echo "7. 查看当前服务状态"
         echo "8. 运行日志查询"
         echo "9. 删除节点"
-        echo "10. 质押代币"  
+        echo "10. 质押代币"
         echo "0. 退出脚本exit"
         read -r -p "请输入选项: " OPTION
-    
+
         case $OPTION in
         1) install_node ;;
         2) add_wallet ;;
         3) import_wallet ;;
         4) check_balances ;;
-        5) check_sync_status ;;    
+        5) check_sync_status ;;
         6) add_validator ;;
         7) check_service_status ;;
         8) view_logs ;;
         9) uninstall_node ;;
-        10) delegate_stake ;;  
+        10) delegate_stake ;;
         0) echo "退出脚本。"; exit 0 ;;
         *) echo "无效选项，请重新输入。"; sleep 3 ;;
         esac
